4c3c866e66e8d2c7fc9b6ec03532b3e49823c50c
Sam Mailand <sam.mailand@gmail.com>
2014-03-23 15:13:21 -0400

    Impliments a blank tab/planning poker module based on the tutorial that is oneline.

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/PlanningPoker/PlanningPoker.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/PlanningPoker/PlanningPoker.java
@@ -1,48 +1,63 @@
+
 package edu.wpi.cs.wpisuitetng.modules.PlanningPoker;
-
-import java.awt.Color;
-import java.util.ArrayList;
+ 
 import java.util.List;
-
-import javax.swing.*;
-
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.util.ArrayList;
+ 
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+ 
 import edu.wpi.cs.wpisuitetng.janeway.modules.IJanewayModule;
 import edu.wpi.cs.wpisuitetng.janeway.modules.JanewayTabModel;
+ 
 
 public class PlanningPoker implements IJanewayModule {
+               
+                private ArrayList<JanewayTabModel> tabs;
+               
+                public PlanningPoker() {
+                    // Initialize the list of tabs (however, this module has only one tab)
+                    tabs = new ArrayList<JanewayTabModel>();
 
-	
-	List<JanewayTabModel> tabs;
-	
-	@Override
-	public String getName() {
-		return "PlanningPoker";
-	}
+                    // Create a JPanel to hold the toolbar for the tab
+                    JPanel toolbarPanel = new JPanel();
+                    toolbarPanel.add(new JLabel("PlanningPoker toolbar placeholder")); // add a label with some placeholder text
+                    toolbarPanel.setBorder(BorderFactory.createLineBorder(Color.blue, 2)); // add a border so you can see the panel
 
-	@Override
-	public List<JanewayTabModel> getTabs() {
-		return tabs;
-	}
-	
-	public PlanningPoker() {
-	    // Initialize the list of tabs (however, this module has only one tab)
-	    tabs = new ArrayList<JanewayTabModel>();
+                    // Create a JPanel to hold the main contents of the tab
+                    JPanel mainPanel = new JPanel();
+                    mainPanel.add(new JLabel("PlanningPoker placeholder"));
+                    mainPanel.setBorder(BorderFactory.createLineBorder(Color.green, 2));
 
-	    // Create a JPanel to hold the toolbar for the tab
-	    JPanel toolbarPanel = new JPanel();
-	    toolbarPanel.add(new JLabel("PlanningPoker toolbar placeholder")); // add a label with some placeholder text
-	    toolbarPanel.setBorder(BorderFactory.createLineBorder(Color.blue, 2)); // add a border so you can see the panel
+                    // Create a tab model that contains the toolbar panel and the main content panel
+                    JanewayTabModel tab1 = new JanewayTabModel(getName(), new ImageIcon(), toolbarPanel, mainPanel);
 
-	    // Create a JPanel to hold the main contents of the tab
-	    JPanel mainPanel = new JPanel();
-	    mainPanel.add(new JLabel("PlanningPoker placeholder"));
-	    mainPanel.setBorder(BorderFactory.createLineBorder(Color.green, 2));
-
-	    // Create a tab model that contains the toolbar panel and the main content panel
-	    JanewayTabModel tab1 = new JanewayTabModel(getName(), new ImageIcon(), toolbarPanel, mainPanel);
-
-	    // Add the tab to the list of tabs owned by this module
-	    tabs.add(tab1);
-	}
-
+                    // Add the tab to the list of tabs owned by this module
+                    tabs.add(tab1);
+                }
+                /*
+                * @see edu.wpi.cs.wpisuitetng.janeway.modules.IJanewayModule#getName()
+                */
+                @Override
+                public String getName() {
+                                // TODO Auto-generated method stub
+                                return "Planning Poker";
+                }
+ 
+                /*
+                * @see edu.wpi.cs.wpisuitetng.janeway.modules.IJanewayModule#getTabs()
+                */
+                @Override
+                public List<JanewayTabModel> getTabs() {
+                                // TODO Auto-generated method stub
+                                return tabs;
+                }
+ 
 }
