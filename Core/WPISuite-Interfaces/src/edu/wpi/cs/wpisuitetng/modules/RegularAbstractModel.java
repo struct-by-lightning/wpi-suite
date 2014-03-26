5567cef65901ce1dbde3bba232c489e72e389642
Barry Biletch <bbiletch@wpi.edu>
2014-03-26 07:11:48 -0400

    Add database code and dummy model to test it

---
+++ Core/WPISuite-Interfaces/src/edu/wpi/cs/wpisuitetng/modules/RegularAbstractModel.java
@@ -0,0 +1,29 @@
+package edu.wpi.cs.wpisuitetng.modules;
+
+public abstract class RegularAbstractModel<CRTP extends RegularAbstractModel<CRTP>>
+		extends AbstractModel {
+	public void save() {
+		return;
+	}
+
+	public void delete() {
+		return;
+	}
+
+	public String toString() {
+		return this.toJSON();
+	}
+
+	@SuppressWarnings("unchecked")
+	public Boolean identify(Object o) {
+		CRTP crtpo;
+		try {
+			crtpo = (CRTP) o;
+		} catch (ClassCastException e) {
+			return false;
+		}
+		return this.identify(crtpo);
+	}
+
+	public abstract Boolean identify(CRTP curious);
+}
