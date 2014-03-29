04d4df7fe03707a7499fc54c930742b3374f37c7
Alec Thompson <ajthompson042@gmail.com>
2014-03-29 13:56:50 -0400

    Adding MailTrap functionality to Mailer I added the functionality for a debug mode in which emails are sent to the MailTrap server.  This is activated by setting the Mailer class field DEBUG to True.  The emails can then be seen on Mailtrap.io by logging in with the email "struct.by.lightning@gmail.com" and the password "team6password". Normal email functionality can be restored by setting DEBUG to False. Committed by: Alec Thompson

--- Janeway/src/edu/wpi/cs/wpisuitetng/janeway/email/Mailer.java
+++ Janeway/src/edu/wpi/cs/wpisuitetng/janeway/email/Mailer.java
@@ -24,8 +24,16 @@ import javax.mail.internet.MimeMessage;
  * 
  */
 public class Mailer {
-	Session session;
-	MimeMessage message;
+	private Session session;
+	private MimeMessage message;
+	private Transport transport;
+
+	// test information
+	private String host = "mailtrap.io";
+	private String login = "structbylightning-17dcc3f2b944376c";
+	private String pass = "ce1a24cb171342c3";
+
+	private final boolean DEBUG = true;
 
 	public Mailer() {
 		session = createSmtpSession();
@@ -33,23 +41,29 @@ public class Mailer {
 		message = new MimeMessage(session);
 
 		try {
+			// testing
+			if (DEBUG)
+				transport = session.getTransport("smtp");
+			// release
 			String from = "struct.by.lightning@gmail.com";
 
 			// set the message to be from struct by lightning
 			message.setFrom(new InternetAddress(from));
 
 			// set the header line
-			message.setSubject("WPI-Suite: Planning Poker");
-			message.setText("A Planning Poker session has begun.");
+			message.setSubject("Great news ladies and gentlemen!");
+			message.setText("Our favorite past time in which we predict the effort in which we must exert our fingers and minds has begun anew! I request that all ye whom have felt the stern blow of Thor's mighty hammer attend the session, so we may reach a general consensus!");
 		} catch (MessagingException mex) {
 			System.out.println("Message creation failed");
 			mex.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Alternate constructor that takes an email as a parameter
-	 * @param emailAddress Hard-coded email address
+	 * 
+	 * @param emailAddress
+	 *            Hard-coded email address
 	 */
 	public Mailer(String emailAddress) {
 		session = createSmtpSession();
@@ -57,15 +71,49 @@ public class Mailer {
 		message = new MimeMessage(session);
 
 		try {
+			// testing
+			if (DEBUG)
+				transport = session.getTransport("smtp");
+			// release
 			String from = "struct.by.lightning@gmail.com";
 
 			// set the message to be from struct by lightning
 			message.setFrom(new InternetAddress(from));
 
 			// set the header line
-			message.setSubject("WPI-Suite: Planning Poker");
-			message.setText("A Planning Poker session has begun.");
+			message.setSubject("Great news ladies and gentlemen!");
+			message.setText("Our favorite past time in which we predict the effort in which we must exert our fingers and minds has begun anew! I request that all ye whom have felt the stern blow of Thor's mighty hammer attend the session, so we may reach a general consensus!");
 			this.addEmail(emailAddress);
+		} catch (MessagingException mex) {
+			System.out.println("Message creation failed");
+			mex.printStackTrace();
+		}
+	}
+
+	/**
+	 * Alternate constructor that takes an array of email addresses as a
+	 * parameter
+	 * 
+	 * @param emailAddress
+	 *            Hard-coded email address
+	 */
+	public Mailer(String[] emailArray) {
+		session = createSmtpSession();
+		session.setDebug(true);
+		message = new MimeMessage(session);
+
+		try {
+			if (DEBUG)
+				transport = session.getTransport("smtp");
+			String from = "struct.by.lightning@gmail.com";
+
+			// set the message to be from struct by lightning
+			message.setFrom(new InternetAddress(from));
+
+			// set the header line
+			message.setSubject("Great news ladies and gentlemen!");
+			message.setText("Our favorite past time in which we predict the effort in which we must exert our fingers and minds has begun anew! I request that all ye whom have felt the stern blow of Thor's mighty hammer attend the session, so we may reach a general consensus!");
+			this.addEmailArray(emailArray);
 		} catch (MessagingException mex) {
 			System.out.println("Message creation failed");
 			mex.printStackTrace();
@@ -79,22 +127,35 @@ public class Mailer {
 	 * @return Google SMTP session
 	 */
 	private Session createSmtpSession() {
-		final Properties props = new Properties();
-		props.setProperty("mail.smtp.host", "smtp.gmail.com");
-		props.setProperty("mail.smtp.auth", "true");
-		props.setProperty("mail.smtp.port", "" + 587);
-		props.setProperty("mail.smtp.starttls.enable", "true");
-		props.setProperty("mail.transport.protocol", "smtp");
+		// final version settings
+		if (DEBUG) {
+			// test with mailtrap
+			Properties props = System.getProperties();
+			props.put("mail.smtp.host", host);
+			props.put("mail.smtp.user", login);
+			props.put("mail.smtp.password", pass);
+			props.put("mail.smtp.port", "2525");
+			props.put("mail.smtp.auth", "true");
 
-		return Session.getDefaultInstance(props,
-				new javax.mail.Authenticator() {
+			return Session.getDefaultInstance(props);
+		} else {
+			final Properties props = new Properties();
+			props.setProperty("mail.smtp.host", "smtp.gmail.com");
+			props.setProperty("mail.smtp.auth", "true");
+			props.setProperty("mail.smtp.port", "" + 587);
+			props.setProperty("mail.smtp.starttls.enable", "true");
+			props.setProperty("mail.transport.protocol", "smtp");
 
-					protected PasswordAuthentication getPasswordAuthentication() {
-						return new PasswordAuthentication(
-								"struct.by.lightning@gmail.com",
-								"Donthackthis!12358");
-					}
-				});
+			return Session.getDefaultInstance(props,
+					new javax.mail.Authenticator() {
+
+						protected PasswordAuthentication getPasswordAuthentication() {
+							return new PasswordAuthentication(
+									"struct.by.lightning@gmail.com",
+									"Donthackthis!12358");
+						}
+					});
+		}
 	}
 
 	/**
@@ -104,10 +165,19 @@ public class Mailer {
 	 */
 	public boolean send() {
 		try {
+			if (DEBUG)
+				transport.connect(host, login, pass);
 			// send the message
 			System.out.println("Ready to send message");
-			Transport.send(message);
+			// testing
+			if (DEBUG) {
+				transport.sendMessage(message, message.getAllRecipients());
+			} else { // release
+				Transport.send(message);
+			}
 			System.out.println("Sent message successfully");
+			if (DEBUG)
+				transport.close();
 			return true;
 		} catch (MessagingException mex) {
 			System.out.println("Send failed");
