14f36f843039c6b32e2fdbc29d06d1303d764107
Ryan Baylor Killea <gsmashkilla@gsmashkilla.com>
2014-03-30 16:29:18 -0400

    Add the planningpokermodel and serializer. Also slightly change the test to reflect changes.

--- Core/WPISuite-Interfaces/src/edu/wpi/cs/wpisuitetng/modules/RegularAbstractModel.java
+++ Core/WPISuite-Interfaces/src/edu/wpi/cs/wpisuitetng/modules/RegularAbstractModel.java
@@ -25,7 +25,11 @@ public abstract class RegularAbstractModel<CRTP extends RegularAbstractModel<CRT
 		return this.identify(crtpo);
 	}
 
-	public abstract Boolean identify(CRTP curious);
+	public Boolean identify(CRTP curious) {
+		return this.getID() == curious.getID();
+	}
+	
 	public abstract String getID();
-	public static String primaryKey;
+	public abstract void setID(String toSet);
+	public abstract String getPrimaryKey();
 }
