2a13d3a96d07651cedc6095ff5e1618d7e9d4c39
Batyr <batyrlan94@gmail.com>
2014-03-30 17:43:47 -0400

    Author: bnurbekov, cgwalker, eVulmaNn, swconley, mmora
    Adds two buttons to the toolbar panel: "New Game" and "Modify Game"
    
    Adds two buttons to the toolbar panel that allow to create and modify game
    for the moderator

---
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/PlanningPoker/buttons/PlanningPokerButtonsPanel.java
@@ -0,0 +1,107 @@
+/*******************************************************************************
+ * Copyright (c) 2013 WPI-Suite
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Team Rolling Thunder
+ ******************************************************************************/
+package edu.wpi.cs.wpisuitetng.modules.PlanningPoker.buttons;
+
+import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.AbstractButton;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.SpringLayout;
+import javax.swing.SwingConstants;
+import javax.swing.border.EtchedBorder;
+
+import edu.wpi.cs.wpisuitetng.janeway.gui.container.toolbar.ToolbarGroupView;
+//import edu.wpi.cs.wpisuitetng.modules.requirementmanager.view.ViewEventController;
+
+/**
+ * @author Batyr, Christian, Francisco 
+ * @version $Revision: 1.0 $
+ */
+public class PlanningPokerButtonsPanel extends ToolbarGroupView{
+	
+	// initialize the main view toolbar buttons
+		private JButton newGameButton = new JButton("<html>New<br />Game</html>");
+		
+		private final JButton modifyGameButton = new JButton("<html>Modify<br />Game</html>");
+		private final JPanel contentPanel = new JPanel();
+	
+	public PlanningPokerButtonsPanel(){
+		super("");
+		
+		this.contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.X_AXIS));
+		this.setPreferredWidth(350);
+		
+		//this.modifyGameButton.setSize(200, 200);
+		//this.newGameButton.setPreferredSize(new Dimension(200, 200));
+		this.newGameButton.setHorizontalAlignment(SwingConstants.CENTER);
+		/*try {
+		    Image img = ImageIO.read(getClass().getResource("new_req.png"));
+		    this.newGameButton.setIcon(new ImageIcon(img));
+		    
+		    img = ImageIO.read(getClass().getResource("new_itt.png"));
+		    this.modifyGameButton.setIcon(new ImageIcon(img));
+		    
+		} catch (IOException ex) {}*/
+		
+		// the action listener for the Create Requirement Button
+		newGameButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// bring up a create requirement pane if not in Multiple Requirement Editing Mode
+				//if (!ViewEventController.getInstance().getOverviewTable().getEditFlag()) {
+					//ViewEventController.getInstance().createRequirement();
+			//	}
+			}
+		});		
+		
+		//action listener for the Create Iteration Button
+		modifyGameButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//if (!ViewEventController.getInstance().getOverviewTable().getEditFlag()) {
+					//ViewEventController.getInstance().createIteration();
+				}
+		//	}
+		});
+			
+		contentPanel.add(newGameButton);
+		contentPanel.add(modifyGameButton);
+		contentPanel.setOpaque(false);
+		
+
+		this.add(contentPanel);
+	}
+	/**
+	 * Method getnewGameButton.
+	
+	 * @return JButton */
+	public JButton getnewGameButton() {
+		return newGameButton;
+	}
+
+	/**
+	 * Method getmodifyGameButton.
+	
+	 * @return JButton */
+	public JButton getmodifyGameButton() {
+		return modifyGameButton;
+	}
+
+	
+}
--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/PlanningPoker/view/ToolbarPanel.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/PlanningPoker/view/ToolbarPanel.java
@@ -1,18 +1,25 @@
 package edu.wpi.cs.wpisuitetng.modules.PlanningPoker.view;
 
-import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.event.ActionEvent;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
 import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JLabel;
+
+import java.awt.Component;
 
 import javax.swing.Box;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+
+import java.awt.Dimension;
+import java.awt.Font;
+
 import javax.swing.SwingConstants;
 
-import edu.wpi.cs.wpisuitetng.janeway.email.Mailer;
+import edu.wpi.cs.wpisuitetng.modules.PlanningPoker.buttons.PlanningPokerButtonsPanel;
+import edu.wpi.cs.wpisuitetng.janeway.gui.container.toolbar.DefaultToolbarView;
+import edu.wpi.cs.wpisuitetng.janeway.gui.container.toolbar.ToolbarGroupView;
 
 /**
  * This panel contains the refresh button
@@ -25,28 +32,28 @@ import edu.wpi.cs.wpisuitetng.janeway.email.Mailer;
  *
  */
 @SuppressWarnings("serial")
-public class ToolbarPanel extends JPanel {
+public class ToolbarPanel extends DefaultToolbarView {
 
     /** The refresh button */
-    private final JButton startTimer;
-    private final Mailer mailer;
+    //private final JButton startTimer;
+    PlanningPokerButtonsPanel btnsPanel = new PlanningPokerButtonsPanel();
+
 
     /**
      * Construct the panel.
      */
     public ToolbarPanel() {
-    	mailer = new Mailer("software-team6@wpi.edu");
-    	
+
         // Make this panel transparent, we want to see the JToolbar gradient beneath it
         this.setOpaque(false);
-
-        // Construct the refresh button and add it to this panel
+       
+        /*// Construct the refresh button and add it to this panel
         startTimer = new JButton("Start Timer");
         startTimer.setHorizontalAlignment(SwingConstants.RIGHT);
         startTimer.setFont(new Font("Arial", Font.PLAIN, 15));
         startTimer.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent clicked) {
-        		mailer.send();
+                System.out.println("Timer Started");
         	}
         });
         
@@ -58,8 +65,7 @@ public class ToolbarPanel extends JPanel {
         
         Component rigidArea = Box.createRigidArea(new Dimension(500, 20));
         add(rigidArea);
-        add(startTimer);
-        
-
+        add(startTimer);*/
+        addGroup(btnsPanel);
     }
 }
