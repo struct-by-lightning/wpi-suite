034f47b4dd0473954da44d63e271d39477842634
Zak <zjzapatka@wpi.edu>
2014-04-01 20:55:50 -0400

    Changed MainView structure to resemble RequirementsManager
    Made the structure more simlar to RequirementsManager's MainView class (used tabAdd method to add tab). The structure is also more like RequirementsManager's in the fact that the main panel extends JTabbedPane rather than the previous JPanel, then the content is contained in separate classes.

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/PlanningPoker/view/MainView.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/PlanningPoker/view/MainView.java
@@ -3,7 +3,11 @@ package edu.wpi.cs.wpisuitetng.modules.PlanningPoker.view;
 import javax.swing.JPanel;
 import javax.swing.JSplitPane;
 
+import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Point;
 
 import javax.swing.JLabel;
 import javax.swing.JList;
@@ -15,7 +19,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
+import javax.swing.JPopupMenu;
+import javax.swing.JTabbedPane;
 import javax.swing.JTextPane;
 import javax.swing.JSpinner;
 import javax.swing.JComboBox;
@@ -28,6 +35,8 @@ import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
 import javax.swing.JTable;
 
+import edu.wpi.cs.wpisuitetng.modules.PlanningPoker.view.Overview.OverviewPanel;
+
 /**
  * This panel fills the main content area of the tab for this module. It
  * contains one inner JPanel, the BoardPanel.
@@ -39,93 +48,119 @@ import javax.swing.JTable;
  *
  */
 @SuppressWarnings("serial")
-public class MainView extends JPanel {
+public class MainView extends JTabbedPane {
 	
-	JLabel lblUserStories, lblUsers, lblStorySummary;
+	private boolean dragging = false;
+	private Image tabImage = null;
+	private Point currentMouseLocation = null;
+	private int draggedTabIndex = 0;
+	private OverviewPanel overview = new OverviewPanel();
+	private NewGameTab newGame = new NewGameTab();
+	private Component lastTab = null;
+	private final JPopupMenu popup = new JPopupMenu();
+	private JMenuItem closeAll = new JMenuItem("Close All Tabs");
+	private JMenuItem closeOthers = new JMenuItem("Close Others");
+
+
+
+	/**
+	 * Adds main subtab when user goes to RequirementManager
+	 */
+	public MainView() {
+		this.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
+		this.addTab("Overview", overview);
+		//this.addTab("Iteration Overview", newGame);
+	}
+
+		
+		
+
+
+	/**
+	 * Method paintComponent.
+	 * @param g Graphics
+	 */
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		// Are we dragging?
+		if(dragging && currentMouseLocation != null && tabImage != null) {
+			// Draw the dragged tab
+			g.drawImage(tabImage, currentMouseLocation.x, currentMouseLocation.y, this);
+		}
+	}
+
+
+	/**
+	 * Overridden insertTab function to add the closable tab element.
+	 * 
+	 * @param title	Title of the tab
+	 * @param icon	Icon for the tab
+	 * @param component	The tab
+	 * @param tip	Showing mouse tip when hovering over tab
+	 * @param index	Location of the tab
+	 */
+	@Override
+	public void insertTab(String title, Icon icon, Component component,
+			String tip, int index) {
+		super.insertTab(title, icon, component, tip, index);
+		if (!(component instanceof OverviewPanel) && !(component instanceof IterationOverviewPanel)) {
+			setTabComponentAt(index, new ClosableTabComponent(this));
+		}
+	}
 	
-	JTextArea textArea;
+	/**
+	 * Method getOverview.
 	
-	JLabel lblEstimate;
+	 * @return OverviewPanel */
+	public OverviewPanel getOverview() {
+		return overview;
+	}
 	
-	JComboBox comboBox;
+	/**
+	 * Method setSelectedComponent.
+	 * @param c Component
+	 */
+	@Override
+	public void setSelectedComponent(Component c){
+		this.lastTab = this.getSelectedComponent();
+		super.setSelectedComponent(c);
+	}
+
+	/**
+	 * Method removeTabAt.
+	 * @param i int
+	 */
+	@Override
+	public void removeTabAt(int i){
+		super.removeTabAt(i);
+		try{
+			if (this.lastTab != null){
+				setSelectedComponent(this.lastTab);}
+		} catch (IllegalArgumentException e){}
+	}
 	
-	JLabel lblStatistics;
-	
-	JTextArea statistics_text;
-	
-	JButton btnSubmit;
-	private JTable userStoryTable;
-	private JTable usersTable;
-    public MainView() {
-    	setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
-    	setLayout(null);
-    	
-    	lblUserStories = new JLabel("User Stories");
-    	lblUserStories.setFont(new Font("Arial", Font.PLAIN, 20));
-    	lblUserStories.setBounds(100, 24, 119, 25);
-    	add(lblUserStories);
-    	
-    	lblUsers = new JLabel("Users");
-    	lblUsers.setFont(new Font("Arial", Font.PLAIN, 20));
-    	lblUsers.setBounds(364, 24, 60, 25);
-    	add(lblUsers);
-    	
-    	lblStorySummary = new JLabel("Story Summary");
-    	lblStorySummary.setFont(new Font("Arial", Font.PLAIN, 20));
-    	lblStorySummary.setBounds(562, 24, 137, 25);
-    	add(lblStorySummary);
-    	
-    	textArea = new JTextArea();
-    	textArea.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-    	textArea.setEditable(false);
-    	textArea.setBounds(511, 61, 239, 132);
-    	add(textArea);
-    	
-    	lblEstimate = new JLabel("Estimate");
-    	lblEstimate.setFont(new Font("Arial", Font.PLAIN, 20));
-    	lblEstimate.setBounds(511, 223, 89, 25);
-    	add(lblEstimate);
-    	
-    	//Changed to final because I couldn't grab the data otherwise
-    	comboBox = new JComboBox();
-    	comboBox.setModel(new DefaultComboBoxModel(new String[] {"0", "1", "2", "3", "5", "8", "13", "20", "40", "100"}));
-    	comboBox.setBounds(511, 259, 77, 20);
-    	add(comboBox);
-    	
-    	btnSubmit = new JButton("Submit");
-    	btnSubmit.setBounds(623, 227, 127, 52);
-    	add(btnSubmit);
-    	
-    	lblStatistics = new JLabel("Statistics");
-    	lblStatistics.setFont(new Font("Arial", Font.PLAIN, 20));
-    	lblStatistics.setBounds(586, 301, 89, 25);
-    	add(lblStatistics);
-    	
-    	statistics_text = new JTextArea();
-    	statistics_text.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-    	statistics_text.setEditable(false);
-    	statistics_text.setBounds(511, 338, 239, 161);
-    	add(statistics_text);
-    	
-    	userStoryTable = new JTable();
-    	userStoryTable.setBounds(32, 60, 251, 439);
-    	add(userStoryTable);
-    	
-    	usersTable = new JTable();
-    	usersTable.setBounds(330, 61, 137, 439);
-    	add(usersTable);
-    	
-    	
-    	
-    	//When the "Submit button is pushed"
-    	btnSubmit.addActionListener(new ActionListener() {
-    		  public void actionPerformed(ActionEvent evt) {
-    			//Grabs the selected answer from the drop down and prints them out in the console
-    		    String selectedValue = (String)comboBox.getSelectedItem();
-    		    System.out.println("SELECTED: " + selectedValue);
-    		    statistics_text.setText("");
-    		    statistics_text.append(selectedValue);
-    		  }
-    		});
-    }
+	/**
+	 * Method getPopup.
+	 * @return JPopupMenu
+	 */
+	public JPopupMenu getPopup() {
+		return popup;
+	}
+	/**
+	 * Method getCloseAll.
+	 * @return JMenuItem
+	 */
+	public JMenuItem getCloseAll() {
+		return closeAll;
+	}
+
+
+	/**
+	 * Method getCloseOthers.
+	 * @return JMenuItem
+	 */
+	public JMenuItem getCloseOthers() {
+		return closeOthers;
+	}
 }
