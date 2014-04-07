b07cf5e712e928c2dc35dffedbae3d1e1ca5e334
Alec Thompson <ajthompson042@gmail.com>
2014-04-06 22:44:36 -0400

    Added preliminary mock objects Added a preliminary mock datastore, network, and requests.  If the requirement manager tests make sense, the mock network and request should be fine as-is.  Stuff still needs to be added to the mock datastore, but I need to look at what we need to add test specifically to figure out if these bare-bones mock objects are enough.  And that's a job for when I'm more rested.
    Committed by: Alec Thompson - ajthompson

---
+++ PlanningPoker/test/edu/wpi/cs/wpisuitetng/modules/PlanningPoker/mockobjects/MockNetwork.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2014 -- WPI Suite
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: team struct-by-lightning
+ *******************************************************************************/
+package edu.wpi.cs.wpisuitetng.modules.PlanningPoker.mockobjects;
+
+import edu.wpi.cs.wpisuitetng.network.Network;
+import edu.wpi.cs.wpisuitetng.network.Request;
+import edu.wpi.cs.wpisuitetng.network.models.HttpMethod;
+
+/**
+ * Description
+ * 
+ * @author Alec Thompson - ajthompson
+ * @version Apr 6, 2014
+ */
+public class MockNetwork extends Network {
+	protected MockRequest lastRequest = null;
+
+	@Override
+	public Request makeRequest(String path, HttpMethod requestMethod) {
+		if (requestMethod == null) {
+			throw new NullPointerException("requestMethot may not be null");
+		}
+
+		lastRequest = new MockRequest(defaultNetworkConfiguration, path,
+				requestMethod);
+
+		return lastRequest;
+	}
+
+	/**
+	 * @return the last MockRequest made
+	 */
+	public MockRequest getLastRequest() {
+		return lastRequest;
+	}
+}
