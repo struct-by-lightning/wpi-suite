149d9c9b14114d9e53ad7b137f9492dd5217dbc1
Zak <zjzapatka@wpi.edu>
2014-04-07 19:58:48 -0400

    Made add/remove buttons uniform
    Central add/remove buttons are now uniform over any window size/resolution

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/PlanningPoker/view/NewGameTab.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/PlanningPoker/view/NewGameTab.java
@@ -94,6 +94,8 @@ import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.characteristics.
 import java.awt.Insets;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
 
 /**
  * Implements the new game tab for planning poker module
@@ -487,15 +489,12 @@ public class NewGameTab extends JPanel {
 		projectList.add(allRequirements);
 
 		JPanel addRemPanel = new JPanel();
-		addRemPanel.setBorder(new LineBorder(Color.LIGHT_GRAY));
+		addRemPanel.setBorder(new LineBorder(new Color(192, 192, 192), 0));
 		requirementsSelector.add(addRemPanel);
-		addRemPanel.setLayout(new GridLayout(3, 1, 3, 3));
 
 		JPanel topSpacer = new JPanel();
-		addRemPanel.add(topSpacer);
 
 		JPanel buttonsPanel = new JPanel();
-		addRemPanel.add(buttonsPanel);
 		buttonsPanel.setLayout(new GridLayout(4, 1, 4, 4));
 		
 		JPanel topmostButton = new JPanel();
@@ -531,7 +530,23 @@ public class NewGameTab extends JPanel {
 		bottommostButton.add(btn_removeAll);
 
 		JPanel bottomSpacer = new JPanel();
-		addRemPanel.add(bottomSpacer);
+		GroupLayout gl_addRemPanel = new GroupLayout(addRemPanel);
+		gl_addRemPanel.setHorizontalGroup(
+			gl_addRemPanel.createParallelGroup(Alignment.LEADING)
+				.addComponent(topSpacer, GroupLayout.PREFERRED_SIZE, 49, GroupLayout.PREFERRED_SIZE)
+				.addComponent(buttonsPanel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+				.addComponent(bottomSpacer, GroupLayout.PREFERRED_SIZE, 49, GroupLayout.PREFERRED_SIZE)
+		);
+		gl_addRemPanel.setVerticalGroup(
+			gl_addRemPanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_addRemPanel.createSequentialGroup()
+					.addComponent(topSpacer, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+					.addGap(3)
+					.addComponent(buttonsPanel, GroupLayout.PREFERRED_SIZE, 121, GroupLayout.PREFERRED_SIZE)
+					.addGap(3)
+					.addComponent(bottomSpacer, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE))
+		);
+		addRemPanel.setLayout(gl_addRemPanel);
 
 		JPanel gameRequirements = new JPanel();
 		gameRequirements.setBorder(new LineBorder(Color.LIGHT_GRAY));
