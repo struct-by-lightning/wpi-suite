d6447c2f234a10869e184193eeb81eb739b98178
Austin Rose <austin.rose94@gmail.com>
2014-04-07 21:49:34 -0400

    Remove ghost code that snuck its way back in. It was probably @bbiletch.

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/PlanningPoker/view/Overview/OverviewPanel.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/PlanningPoker/view/Overview/OverviewPanel.java
@@ -102,13 +102,5 @@ public class OverviewPanel extends JPanel {
 
 	}
 
-	public void fireRefresh() {
-		GetPlanningPokerGamesController.getInstance().retrievePlanningPokerGames();
-		unanswered.removeAllChildren();
-//		for(PlanningPokerGame game : PlanningPokerGameModel.getInstance().getPlanningPokerGames()) {
-//			unanswered.add(new DefaultMutableTreeNode(game.getGameName()));
-//		}
-	}
-
 
 }
