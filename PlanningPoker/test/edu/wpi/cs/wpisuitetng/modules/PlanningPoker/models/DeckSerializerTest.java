0710578ca6a2ec4abed3846cd5f9408ba528f88d
Alec Thompson <ajthompson042@gmail.com>
2014-04-10 18:28:53 -0400

    Added Serializer Temporary commit to update and merge in a new pull request.
    Submitted by: Alec Thompson - ajthompson

---
+++ PlanningPoker/test/edu/wpi/cs/wpisuitetng/modules/PlanningPoker/models/DeckSerializerTest.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2014 -- WPI Suite
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: team struct-by-lightning
+ *******************************************************************************/
+package edu.wpi.cs.wpisuitetng.modules.PlanningPoker.models;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Description
+ *
+ * @author Alec Thompson - ajthompson
+ * @version Apr 10, 2014
+ */
+public class DeckSerializerTest {
+
+	/**
+	 * Test method for {@link edu.wpi.cs.wpisuitetng.modules.PlanningPoker.models.DeckSerializer#serialize(edu.wpi.cs.wpisuitetng.modules.PlanningPoker.deck.Deck, java.lang.reflect.Type, com.google.gson.JsonSerializationContext)}.
+	 */
+	@Test
+	public void testSerialize() {
+		fail("Not yet implemented");
+	}
+
+}
