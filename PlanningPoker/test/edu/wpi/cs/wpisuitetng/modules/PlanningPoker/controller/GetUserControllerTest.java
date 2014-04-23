a24d421ecb09465f3fbc17da5ca3e1aa6daedd19
cgwalker@wpi.edu <cgwalker@wpi.edu>
2014-04-22 21:19:23 -0400

    Add mock network harness constructor to GetUserControllerTest.
    CoAuthors: Ryan, cgwalker

--- PlanningPoker/test/edu/wpi/cs/wpisuitetng/modules/PlanningPoker/controller/GetUserControllerTest.java
+++ PlanningPoker/test/edu/wpi/cs/wpisuitetng/modules/PlanningPoker/controller/GetUserControllerTest.java
@@ -43,7 +43,10 @@ public class GetUserControllerTest {
 		assertEquals(guc.hashCode(), GetPlanningPokerUserController.getInstance().hashCode());
 	}
 	
-
+	
+	@Test
+	public void testActionPerformed() {
+	}
 
 
 	@Test
