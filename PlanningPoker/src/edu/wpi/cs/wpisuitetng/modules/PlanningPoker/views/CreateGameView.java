ae92db8f50be2db0cfe7342db5ce8cacc4f6c537
sfmailand <sam.mailand@gmail.com>
2014-04-29 14:12:48 -0400

    Remove unused references from CreateGameView

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/PlanningPoker/views/CreateGameView.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/PlanningPoker/views/CreateGameView.java
@@ -61,7 +61,6 @@ import edu.wpi.cs.wpisuitetng.modules.PlanningPoker.models.PlanningPokerGame;
 import edu.wpi.cs.wpisuitetng.modules.PlanningPoker.models.PlanningPokerUser;
 import edu.wpi.cs.wpisuitetng.modules.PlanningPoker.models.PlanningPokerUserModel;
 import edu.wpi.cs.wpisuitetng.modules.PlanningPoker.view.DatePicker;
-import edu.wpi.cs.wpisuitetng.modules.PlanningPoker.view.NewGameTab;
 import edu.wpi.cs.wpisuitetng.modules.requirementmanager.controller.GetRequirementsController;
 import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement;
 import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.RequirementModel;
@@ -371,8 +370,7 @@ public class CreateGameView extends JPanel {
 							final JOptionPane emptyNameErrorPanel = new JOptionPane(
 									"You must enter the session name", JOptionPane.ERROR_MESSAGE);
 							final JDialog errorDialog = emptyNameErrorPanel.createDialog(null);
-							errorDialog.setLocation(thisPanel.getWidth() / 2,
-									thisPanel.getHeight() / 2);
+
 							errorDialog.setVisible(true);
 							btnCreateGame.setEnabled(false);
 						}
@@ -918,7 +916,6 @@ public class CreateGameView extends JPanel {
 	private JButton btn_addToGame;
 	private JButton btn_removeAll;
 	private JTextField sessionName;
-	private NewGameTab thisPanel;
 	private JLabel createGameErrorText;
 	private JPanel titlePanel;
 	private JPanel namePane;
